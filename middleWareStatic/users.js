35fb6d6d50996bc1e1da3555719bffff7b09643d
RenaSalfity <renarenna@hotmail.com>
2025-01-22 23:08:52 +0200

    Add files via upload

---
+++ middleWareStatic/users.js
@@ -0,0 +1,8 @@
+const users = [
+  { username: "admin123", password: "admin123", role: "admin", age: 25 },
+  { username: "admin234", password: "admin234", role: "admin", age: 34 },
+  { username: "user123", password: "user123", role: "user", age: 17 },
+  { username: "user678", password: "user678", role: "user", age: 15 },
+];
+
+  module.exports = users;
